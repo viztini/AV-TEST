559857986f22c7935725f3a4f63d33aa6199672d
viztini <leon.stonehouseburguillo@education.nsw.gov.au>
2025-11-05 08:40:39 +1100

    Create test.bat

---
+++ test.bat
@@ -0,0 +1,5 @@
+@echo off
+echo Testing
+echo X5O!P%%@AP[4\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H+H*>test.txt
+echo Test complete! If your AV hasn't detected this please check for errors.
+pause
